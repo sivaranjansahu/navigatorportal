cc72c7818a2d6a7f6552f5c9cb0ea63b7d70cdec
sivaranjansahu <sivaranjan@outlook.com>
2017-05-16 11:42:34 +0530

    data binding done, all animations working correctly, search page still needs fixes

--- js/main.js
+++ js/main.js
@@ -1,57 +1,61 @@
+
+
 $(document).ready(function () {
+    window.onload = function () { 
+    $('.preloader').hide('slow');
+ }
     //instantiate a TimelineLite 
-    var firstReversed = false;
-
-    function slideFirst(obj) {
-
-    }
-
     var l1Timeline = new TimelineLite({
         paused: true,
-        //autoRemoveChildren:true
     });
     var l2Timeline = new TimelineLite({
         paused: true,
-        //autoRemoveChildren:true
     });
     var l3Timeline = new TimelineLite({
         paused: true,
-        //autoRemoveChildren:true
     });
+    var initTimeline=new TimelineLite({
+        paused:true
+    })
+
+// Landingpage animations
+initTimeline.staggerFrom('.lvl-0 >img',0.3,{opacity:0,bottom:30},0.5)
+initTimeline.play();
+
     //open close level 0
 
     $('.lvl-0').on('click', function () {
         var offset = $(this);
         var imgheight = $(this).find('img').height();
-        var topOffset = ($(window).height() / 2) + 100;
+        var topOffset = ($(window).height() / 2) - imgheight/2;
         l1Timeline.clear();
         l1Timeline.to('.topmenu', 0.6, {
             top: '-100px',
             ease: Power2.easeOut
         });
-        l1Timeline.set('.topmenu h4', {
+        l1Timeline.set('.topmenu .logo', {
             position: 'fixed',
-            top: 20,
             left: 15,
 
         })
         l1Timeline.to('.topmenu h4', '0.3', {
             position: 'fixed',
-            top: 40,
-            left: 15,
+            top: 5,
+            scale:1.4,
+            left: 40,
             color: '#444',
             fontWeight: 700,
+            fontSize:'20px',
             ease: Power3.easeOut
         })
+        l1Timeline.to($(this).find('p'), 0.3, {
+            opacity: 0
+        }, .14);
         l1Timeline.to($('.overlay'), 0.3, {
             zIndex: 1,
             opacity: 1
         }, .14);
         l1Timeline.set($(this), {
-
-            y: offset.offset.top,
-            x: offset.offset.left,
-            width: offset.width,
             className: "+=currentFixed",
             // className:'-=rested'
 
@@ -59,6 +63,7 @@ $(document).ready(function () {
         var that = this;
         l1Timeline.to($(this), 0.5, {
             className: "+=destFixed",
+            top:'41vh'
         }, -0.1);
         l1Timeline.to($('.triangle'), 1, {
             borderWidthRight: '10vw'
@@ -71,12 +76,7 @@ $(document).ready(function () {
             zIndex: 2,
 
         }, 0.5);
-        l1Timeline.to($(".level-1").find('.closebtn'), 0.3, {
-            rotation: 360,
-            ease: Linear.easeOut
-
-        });
-
+        
         l1Timeline.play();
     })
     // close level 0
@@ -92,9 +92,23 @@ $(document).ready(function () {
                 clearProps: "all"
             }
         })
-        l1Timeline.reverse();
-        l2Timeline.reverse();
         l3Timeline.reverse();
+        l2Timeline.reverse();
+        l1Timeline.reverse();
+        
+        
+    })
+    $(".topmenu h4").on('click', function () {
+         l1Timeline.set($(this), {
+            css: {
+                clearProps: "all"
+            }
+        })
+        l3Timeline.reverse();
+        l2Timeline.reverse();
+        l1Timeline.reverse();
+        
+        
     })
     $(".lvl-0.destFixed.currentFixed").on('click', function () {
         alert();
@@ -106,57 +120,47 @@ $(document).ready(function () {
     // //open close level 1
 
     $('body').on('click','.lvl-1', function () {
-        var offset = $(this);
-        console.log(offset.width())
+        var offset = $(this).offset();
+        //console.log(offset.width())
         var offsetH1 = $(this).parent().find('h1').offset;
         var that = this;
         l2Timeline.clear();
 
-
-        l2Timeline.set($(this), {
-
-            y: offset.offset.top,
-            x: offset.offset.left,
-            width: offset.width(),
-            className: "+=currentFixed"
-
+        l2Timeline.to($(this).parent().parent(), 0.1, {
+            css:{
+                borderWidth:0,
+                backgroundColor:'transparent'
+            }
         });
 
-        //var topOffset = $(window).height() / 2 -($('.lvl-1').find('img').height()/2);
+        l2Timeline.to($(this).parent().parent().find('p'), 0.1, {
+            opacity: 0,
+        }, "-=0.2");
+        l2Timeline.staggerTo($(this).parent().parent().siblings(), 0.2, {
+            opacity: 0
+        }, -0.1);
+
+        l2Timeline.set($(this), {
+            className: "+=currentFixed"
+        },-0.1);
+
         var topOffset = '44vh';
-        l2Timeline.to($(this), 0.2, {
+        //var topOffset = ($(window).height() / 2) -($('.lvl-1').find('img').height()/2) ;
+        l2Timeline.to($(this), 0.5, {
 
             className: "+=destFixed",
             top: topOffset,
-            // height: '80px',
-            // width: '80px',
-            // borderRadius: '40px',
-            // lineHeight: '80px',
+         
+        },-0.1);
 
-
-        });
-        l2Timeline.to($(this).find('p'), 0.1, {
-            opacity: 0,
-            height: 0
-        }, -0.7);
-        l2Timeline.staggerTo($(this).parent().siblings(), 0.5, {
-            opacity: 0
-        }, 0.1);
+        
         l2Timeline.to(".level-2", 0.3, {
             left: "22vw",
             ease: Back.ease,
             opacity: 1,
 
-        }, 1.5);
-        l2Timeline.from($(".level-2").find('.closebtn'), 0.2, {
-            ease: Elastic.easeOut
-
-        });
-        l2Timeline.to($(".level-2").find('.closebtn'), 0.3, {
-            rotation: 360,
-            ease: Power1.easeOut
-
-        });
+        }, "+=0.3");
+        
         l2Timeline.play();
         l2Timeline.reversed(false)
 
@@ -172,9 +176,8 @@ $(document).ready(function () {
                 clearProps: "all"
             }
         });
-
-        l2Timeline.reverse();
         l3Timeline.reverse();
+        l2Timeline.reverse();
         l2Timeline.reversed(true)
         console.log(l2Timeline.reversed());
         //$(this).off('click');
@@ -188,33 +191,31 @@ $(document).ready(function () {
         var that = this;
         l3Timeline.clear();
         var topOffset = $(window).height() / 2 - ($(this).find('img').height() / 2);
-
+        l3Timeline.to($(this).parent().parent(), 0.1, {
+            css:{
+                borderWidth:0
+            }
+        }, -0.7);
+        l3Timeline.to($(this).parent().parent().find('p'), 0.1, {
+            opacity: 0,
+            height: 0
+        }, -0.7);
+        l3Timeline.staggerTo($(this).parent().parent().siblings(), 0.1, {
+            opacity: 0,
+            right: 20
+        }, 0.1);
         l3Timeline.set($(this), {
-
-            y: offset.offset.top,
-            x: offset.offset.left,
-            width: offset.width(),
             className: "+=currentFixed"
 
-        });
+        },-0.1);
         l3Timeline.to($(this), 0.2, {
 
             className: "+=destFixed",
             top: '46vh',
-            height: '60px',
-            width: '60px',
-            borderRadius: '30px',
-            lineHeight: '60px'
+
 
         });
-        l3Timeline.to($(this).find('p'), 0.1, {
-            opacity: 0,
-            height: 0
-        }, -0.7);
-        l3Timeline.staggerTo($(this).parent().siblings(), 0.1, {
-            opacity: 0,
-            right: 20
-        }, 0.1);
+        
 
         l3Timeline.to(".level-3", 0.3, {
             left: "32vw",
@@ -222,18 +223,7 @@ $(document).ready(function () {
             opacity: 1,
 
         }, 1.5);
-        l3Timeline.from($(".level-3").find('.closebtn'), 0.5, {
-            // css:{
-            //     scale:0.0001
-            // },
-            ease: Elastic.easeOut
-
-        });
-        l3Timeline.to($(".level-3").find('.closebtn'), 0.5, {
-            rotation: 360,
-            ease: Power1.easeOut
-
-        });
+        
         l3Timeline.play();
         l3Timeline.reversed(false)
 
@@ -256,6 +246,21 @@ $(document).ready(function () {
         //$(this).off('click');
     })
 
+    // $('body').on('mouseover','.col-item',function(){
+    //     $(this).addClass('col-item__dark')
+    //     //TweenLite.set(this,{className:'+=col-item__dark'})
+    // })
+    // $('body').on('mouseout','.col-item',function(){
+    //     $(this).removeClass('col-item__dark')
+    //      //TweenLite.set(this,{className:'-=col-item__dark'})
+    // })
+
+//     $('.col-item').hover(function(){
+//         alert();
+	
+// },function(){
+// 	TweenLite.to(this,1,{scale:11,ease:Elastic.ease})
+// })
 
     // Search Animations
 
@@ -289,7 +294,7 @@ $(document).ready(function () {
         $(this).removeClass('animated pulse')
     })
 
-
+ 
 })
 var myFunc = function (event) {
     event.stopPropagation();
